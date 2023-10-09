cb8f24480d966663522f57d7870159920d32e4ed
Harshith <harshalva.titan@csu.fullerton.edu>
2023-10-08 18:17:34 -0700

    First version

--- src/page/navi.js
+++ src/page/navi.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import '../App.css';
+import { Link } from 'react-router-dom';
 
 const Navi = () => {
     return(
@@ -13,9 +14,9 @@ const Navi = () => {
             <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
                 <div class="navbar-nav">
                     <a class="nav-link active" aria-current="page" href="#">Home</a>
-                    <a class="nav-link" href="#">Features</a>
-                    <a class="nav-link" href="#">Pricing</a>
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                    <Link class="nav-link" to = "/about">About</Link>
+                    <Link class = "nav-link" to="/projects" > Projects </Link> 
+                    <a class="nav-link" href="#" tabindex="-1">Resume</a>
                 </div>
             </div>
         </div>
