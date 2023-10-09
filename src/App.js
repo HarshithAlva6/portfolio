967a42ab437d5884967e888d0759f2560413d85e
Harshith <harshalva.titan@csu.fullerton.edu>
2023-10-08 17:08:38 -0700

    Aligned the project section

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Info from './page/info';
 import Projects from './page/projects';
+import Navi from './page/navi';
 import './App.css';
 
 function App() {
   return ( 
     <div className="App">
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous"></link>
+        <Navi />
         <Info />
         <Projects />
     </div>
