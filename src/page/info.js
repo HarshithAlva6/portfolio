59c68e6c721dd06c80d32aa571d5b2e4969adb4d
Harshith <harshalva.titan@csu.fullerton.edu>
2025-07-01 12:40:17 -0700

    About update

--- src/page/info.js
+++ src/page/info.js
@@ -7,15 +7,16 @@ const Info = () => {
         <div class="info">
         <div>
             <h1 className = "my-heading">WELCOME.</h1>
-            <p class = "text-white">I am <strong>Harshith Harijeevan</strong>, currently situated at <b>Austin, Texas</b>, but originally from <b>Bengaluru</b>, the Silicon Valley of India! <br />
+            <p class = "text-white">I am <strong>Harshith Harijeevan</strong>, also go by the name Harshith Alva, currently situated at <b>Austin, Texas</b>.<br />
+                My roots originate from Tulunadu, born in Maharashtra, but originally from <b>Bengaluru</b>, the Silicon Valley of India! <br />
                 A <i>Full Stack Web Developer</i> by day, and an enthusiastic learner by night! &#128513; <br />
                 Currently having completed my Masters in <i>Computer Science</i> at California State University, Fullerton.
             </p>
         </div>
             <p class = "text-white"><strong>Innovative Full Stack developer with 3+ years of experience in efficient, robust and scalable web applications.
-Proficient in JavaScript (React, Next.js), Python, AWS and SQL, with a strong background in web design and
-optimising the end-user experience. Passionate about staying at the forefront of emerging web technologies.
-</strong></p>
+            Proficient in JavaScript (React, Next.js), Python, AWS and SQL, with a strong background in web design and
+            optimising the end-user experience. Passionate about staying at the forefront of emerging web technologies.
+            </strong></p>
         </div>
     </div>
     );
